229e243753e5113961cb3479f272335d58107d39
daniel stern <daniel@danielstern.ca>
2013-10-11 09:43:38 -0400

    before clean up

--- game-option.directive.js
+++ game-option.directive.js
@@ -1,15 +1,20 @@
 define(['app'] , function (app) {
-	app.directive('gameOption', function ($compile) {
+	app.directive('gameOption', function ($compile, $http) {
     return {
       restrict: 'A',
       scope: {
         page:'=',
       },
       link: function (scope, elem, attrs) {
-      //  console.log("Detected game-option...", elem, scope.page);
-          elem.bind('click', function(e){
             var el = angular.element("<div story-part url='"+scope.page+"'></div>")
-            var next = $compile(el)
+            $http({method: "GET", url: 'story/' +scope.page +'.html',})
+            .error(function(data, status) {
+             
+              elem.attr('disabled',true)
+          });
+          elem.bind('click', function(e){
+
+             var next = $compile(el)
               elem.after(el);
               next(scope);
 
