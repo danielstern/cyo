b8a35b473ffee68cbf366cfbbf1e657874f09f0d
daniel stern <daniel@danielstern.ca>
2013-10-11 01:29:24 -0400

    got template url hummin

--- game-option.directive.js
+++ game-option.directive.js
@@ -8,16 +8,13 @@ define(['app'] , function (app) {
       link: function (scope, elem, attrs) {
         console.log("Detected game-option...", elem, scope.page);
           elem.bind('click', function(e){
-    //        var storyNextPart = ss.getPage(scope.page);
-          //var el = 
-          var el = angular.element("<div story-part>123445</div>")
-          var next = $compile("<div story-part></div>")
-          console.log("Next?",next)
-            elem.append(el);
-            next(scope)
-          })
-
-        },
+            var el = angular.element("<div story-part>123445</div>")
+            var next = $compile(el)
+          //  console.log("Next?",next)
+              elem.after(el);
+              next(scope)
+           })
+      },
     }
   });
 })
--- storyPart.js
+++ storyPart.js
@@ -1,16 +1,17 @@
 define(['app'] , function (app) {
-	app.directive('storyPart', ['storytellingService', function (ss) {
+	app.directive('storyPart', function () {
     return {
       restrict: 'A',
+      replace:true,
       scope: {
+        url:'=',
       },
+      //template:"<div ng-include='\"story/"+"001"+".html\"'></div>",
+      templateUrl:"story/001.html",
       link: function (scope, elem, attrs) {
-        console.log("Detected story part...", elem, scope.page);
-          elem.bind('click', function(){
-           
-          })
+        console.log("Detected story part...", scope.url);
 
         },
     }
-  }]);
+  });
 })
