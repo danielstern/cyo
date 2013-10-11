2866c9532c8e33ba5ea31ed214a30e144c7059bf
unknown <daniel@danielstern.ca>
2013-10-11 15:54:27 -0700

    all working

--- storytelling/game-option.directive.js
+++ storytelling/game-option.directive.js
@@ -1,16 +1,22 @@
 define(['app'] , function (app) {
-	app.directive('gameOption', function ($compile, $http) {
+	app.directive('choice', function ($compile, $http) {
     return {
-      restrict: 'A',
+      restrict: 'E',
       scope: {
         page:'=',
       },
+      template:function(a,b){
+        console.log("A,B?",a,b)
+        return "<button class='btn btn-default'>"+a.html()+"</button>";
+    },
       link: function (scope, elem, attrs) {
             var el = angular.element("<chapter url='"+scope.page+"'></chapter>")
             $http({method: "GET", url: 'story/' +scope.page +'.html',})
             .error(function(data, status) {
              
-              elem.attr('disabled',true)
+              var btn = elem.find('button');
+              btn.attr('disabled',true)
+              btn.addClass('btn-disabled')
           });
           elem.bind('click', function(e){
 
