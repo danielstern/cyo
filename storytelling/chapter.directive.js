741a0cc32529e013de299944f3f51108fe44dda5
unknown <daniel@danielstern.ca>
2013-10-14 13:36:07 -0400

    refactoring good

--- storytelling/chapter.directive.js
+++ storytelling/chapter.directive.js
@@ -7,22 +7,19 @@ define(['app','underscore','css!global/styles.css'] , function (app, _) {
         url:'=',
       },
       templateUrl:function(elem, atts){
-      	console.log("Templateurl...",atts);
+
       	var url = atts.url;
       	url = url || _.dasherize(_.keysToKeyword(atts.$attr));
         return "story/"+ url +".html";
+
       },
       link: function (scope, elem, attrs) {
 
-     // 	var url;
-
-      	//scope.url = scope.url || _.dasherize(_.keysToKeyword(attrs.$attr));
-
-         // hacky
          scope.buttons = elem.find('button');
          $(scope.buttons).click(function(){
-              $(scope.buttons).hide();
-              //elem.addClass('chapter-fade');
+
+              $(scope.buttons).hide();  // todo: fix
+
             })
 
 					elem.addClass('pull-down-in');
